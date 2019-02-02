1545f66fae2256dd0106e11ad8eff4db653839bd
Ismael Bonneau <ismael95210@gmail.com>
2019-02-02 19:28:43 +0100

    cree srtparser.py

---
+++ cleaningdata/srtparser.py
@@ -0,0 +1,97 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*
+
+# Ismael Bonneau & Issam Benamara
+
+#fichier servant à nettoyer un fichier .srt 
+#et récupérer les lignes de dialogue le composant.
+
+import re
+
+url_regex = r'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+'
+timestamp_regex = r'(2[0-3]|[01][0-9]|[0-9]):([0-5][0-9]|[0-9]):([0-5][0-9]|[0-9])'
+startingTag_regex = r'<[uib]+>'
+closingTag_regex = r'</[uib]+>'
+uselessTag_regex = r'<[a-zA-Z]+ .*>'
+
+#les fichiers .srt peuvent contenir des balises qui encadrent du texte:
+# - gras : <b>...</b> 
+# - italique : <i>...</i> 
+# - souligné : <u>...</u>
+
+#d'autres balises comme des balises de font (police d'écriture) peuvent aussi être présentes
+#il faut également se débarrasser des crédits (translated by ***, uploaded by ***, etc)
+
+def containsUselessURL(line):
+	return bool(re.search(uselessTag_regex, line))
+
+def containsURL(line):
+	return bool(re.search(url_regex, line)) or bool(re.search(r'www', line))
+
+def startingTag(line):
+	return bool(re.search(startingTag_regex, line))
+
+def closingTag(line):
+	return bool(re.search(closingTag_regex, line))
+
+def islowercase(char):
+	return (char.lower() == char and char.isalpha())
+
+def firstPerson(line):
+	"""
+	renvoie True si la ligne commence par un I
+	-le seul cas en anglais où une majuscule ne commence par la phrase-
+	suppose une phrase en anglais.
+	"""
+	if line[0] == "I" and line[1] in " '":
+		return True
+	return False
+
+
+def timestamp(line):
+	"""
+	renvoie True si la ligne contient un timestamp
+	"""
+	return bool(re.match(timestamp_regex, line))
+
+def istext(line):
+  if re.search('[a-zA-Z]', line):
+    return True
+  return False
+
+
+def parse(filename):
+	"""
+	parse un fichier .srt pour ne garder que les lignes de dialogue.
+	"""
+
+	#A COMPLETER
+
+	with open(filename, "r") as file:
+		strings = file.readlines()
+
+		newlines = []
+		i = 0
+		current_sentence = ""
+		while i < len(strings):
+			line = strings[i]
+			#ne garder que les lignes de texte
+			if not(timestamp(line)) and (istext(line)):
+				text = line.rstrip()
+
+				if not(containsURL(text)) and not(containsUselessURL(text)):
+
+					text = re.sub(startingTag_regex,'', text)
+					text = re.sub(closingTag_regex,'', text)
+
+					#dans ce cas on considère comme faisant partie de la ligne précédente
+					if islowercase(text[0]) or (text[0] == ','):
+						newlines[-1] = newlines[-1] + " " + text
+					elif firstPerson(text) and newlines[-1][-1] not in ".?":
+						newlines[-1] = newlines[-1] + " " + text
+					else:
+						newlines.append(text)
+			i += 1
+
+		for line in newlines:
+			print(line)
