730e185ec17c72e6b21c0bd0a6a998bc5cfbcbba
Ismael Bonneau <ismael95210@gmail.com>
2019-02-03 21:16:31 +0100

    ameliorations

--- classifiers/languageClassifier.py
+++ classifiers/languageClassifier.py
@@ -19,18 +19,16 @@ def quickLanguageDetector(text):
 	marche de façon très simple: regarde combien de stopwords
 	d'un langage donné le texte contient.
 	moins précis qu'un calcul de stopwords sur le texte mais plus rapide
-	nécessite une entrée en utf-8
+	nécessite une entrée en utf 8
 	"""
 	tokens_set = wordpunct_tokenize(text)
 	tokens_set = set([token.lower() for token in tokens_set])
 
-	response = np.array([0,0,0,0,0,0,0,0,0])
-	i = 0
+	response = []
 	for language in languages:
 		stopwords_set = set(stopwords.words(language))
-		response[i] = len(tokens_set.intersection(stopwords_set))
-		i+=1
-	return languages[np.argmax(response)]
+		response.append(len(tokens_set.intersection(stopwords_set)))
+	return languages[np.argmax(np.array(response))]
 
 
 def languageDetector(text):
@@ -44,19 +42,20 @@ def languageDetector(text):
 	tokens = [token.lower() for token in wordpunct_tokenize(text) if token not in string.punctuation ]
 	fdist = nltk.FreqDist(tokens)
 
-	print(fdist.most_common(30))
-	response = np.array([0,0,0,0,0,0,0,0,0])
-	i = 0
+	response = []
 	for language in languages:
-		stpwords = stopwords.words(language)
+		stpwords = stopwords.words(language) 
 		text_stopwords_set = set()
 		for word, frequence in fdist.most_common(len(stpwords)):
 			text_stopwords_set.add(word)
-		stopwords_set = set(stopwords.words(language))
-		response[i] = len(text_stopwords_set.intersection(stopwords_set))
-		i+=1
-	return languages[np.argmax(response)]	
+		stopwords_set = set(stpwords)
+		response.append(len(text_stopwords_set.intersection(stopwords_set)) / len(stopwords_set))
+
+	return languages[np.argmax(np.array(response))]	
 
 
+filename = "01__1______.lines"
 
-
+with codecs.open(filename, "r", "utf-8") as f:
+	text = f.read()
+	print(quickLanguageDetector(text))
