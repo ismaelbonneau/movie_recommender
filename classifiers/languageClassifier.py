b6cc07d774ffa79d4c057a2fdec84269b3077bf8
Ismael Bonneau <ismael95210@gmail.com>
2019-02-03 19:35:05 +0100

    avancement

---
+++ classifiers/languageClassifier.py
@@ -0,0 +1,10 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*
+
+# Ismael Bonneau & Issam Benamara
+
+from nltk.corpus import stopwords
+
+filepath = "00__A_Gathering_Storm.lines"
+
+languages = [u'english', u'french', u'spanish', u'russian', u'italian', u'portuguese', u'german']
