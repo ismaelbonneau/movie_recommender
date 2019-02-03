33ed11cc4376375a037dace615292fc0dce479b4
Ismael Bonneau <ismael95210@gmail.com>
2019-02-03 20:17:35 +0100

    avancement

--- classifiers/languageClassifier.py
+++ classifiers/languageClassifier.py
@@ -4,7 +4,29 @@
 # Ismael Bonneau & Issam Benamara
 
 from nltk.corpus import stopwords
+from nltk import wordpunct_tokenize
+import numpy as np
 
-filepath = "00__A_Gathering_Storm.lines"
 
-languages = [u'english', u'french', u'spanish', u'russian', u'italian', u'portuguese', u'german']
+languages = ['english', 'french', 'spanish', 'russian', 'italian', 'portuguese', 'german', 'turkish', 'arabic']
+
+def quickLanguageDetector(text):
+	"""
+	detecte le langage d'un texte.
+	marche de façon très simple: regarde combien de stopwords
+	d'un langage donné le texte contient.
+	moins précis qu'un calcul de stopwords sur le texte mais plus rapide
+	"""
+	tokens_set = wordpunct_tokenize(text)
+	tokens_set = set([token.lower() for token in tokens_set])
+
+	response = np.array([0,0,0,0,0,0,0,0,0])
+	i = 0
+	for language in languages:
+		stopwords_set = set(stopwords.words(language))
+
+		print("intersection avec "+language+" : "+str(len(tokens_set.intersection(stopwords_set))))
+		response[i] = len(tokens_set.intersection(stopwords_set))
+		i+=1
+	return languages[np.argmax(response)]
+
