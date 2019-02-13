e5c61a99b5948f5521db33164c50f8b51e29bdff
Ismael Bonneau <ismael95210@gmail.com>
2019-02-13 23:51:21 +0100

    avancement ismael

--- test.py
+++ test.py
@@ -4,7 +4,7 @@
 from collections import Counter
 from utils.featureExtraction import wordLemmatizer
 from utils.load_data import load_data
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn import preprocessing, linear_model, naive_bayes, metrics, svm
 import numpy as np
 
@@ -41,6 +41,8 @@ listeseries = ["2567_House", "2956_Criminal_Minds", "1262_Lost", "175_The_Walkin
 
 listeseries, (X_train, X_test, y_train, y_test) = load_data(path, nbclass=60, split=True, ratio=0.8)
 
+
+# avec countVectorizer
 count_vect = CountVectorizer(analyzer='word', token_pattern=r'\w{1,}', stop_words=stopwords.words('english'), strip_accents='ascii')
 count_vect.fit(X_train + X_test)
 
@@ -62,6 +64,22 @@ errors_dict = getErrors(predictions, y_test, listeseries)
 import pprint
 
 pp = pprint.PrettyPrinter(indent=4)
+#pp.pprint(errors_dict)
 
-pp.pprint(errors_dict)
+
+# avec tf-idf
+del xtrain_count, xtest_count
+
+tfidf_vect = TfidfVectorizer(analyzer='word', token_pattern=r'\w{1,}', stop_words=stopwords.words('english'), max_features=5000, strip_accents='ascii')
+tfidf_vect.fit(X_train + X_test)
+xtrain_tfidf =  tfidf_vect.transform(X_train)
+xtest_tfidf =  tfidf_vect.transform(X_test)
+
+accuracy, predictions = train_model(naive_bayes.MultinomialNB(), xtrain_tfidf, train_y, xtest_tfidf, test_y)
+
+predictions = np.array(predictions)
+y_test = np.array(y_test)
+
+errors_dict = getErrors(predictions, y_test, listeseries)
+#pp.pprint(errors_dict)
 
