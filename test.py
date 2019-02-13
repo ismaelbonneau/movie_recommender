3cf4207afcbc776d807f8ab920bf1aec8c2348cd
Ismael Bonneau <ismael95210@gmail.com>
2019-02-13 23:37:54 +0100

    avancement naive bayes

--- test.py
+++ test.py
@@ -1,22 +1,37 @@
 # !/usr/bin/python
 # -*- coding: utf-8 -
 
+from collections import Counter
 from utils.featureExtraction import wordLemmatizer
 from utils.load_data import load_data
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn import preprocessing, linear_model, naive_bayes, metrics, svm
 import numpy as np
 
+from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 
-
-def train_model(classifier, feature_vector_train, label, feature_vector_valid, test_y):
+def train_model(classifier, X_train, y_train, X_test, y_test):
     
-	classifier.fit(feature_vector_train, label)
+	classifier.fit(X_train, y_train)
 
-	predictions = classifier.predict(feature_vector_valid)
+	predictions = classifier.predict(X_test)
     
-	return metrics.accuracy_score(predictions, test_y)
+	accuracy = metrics.accuracy_score(predictions, y_test)
+
+	print("trained on {} samples, validated on {}.".format(X_train.shape[0], X_test.shape[0]))
+	print("accuracy: ", accuracy)
+
+	return accuracy, predictions
+
+def getErrors(predictions, labels, series):
+	erreurs = predictions != labels
+
+	errors_dict = []
+	for i, v in enumerate(erreurs):
+		if v:
+			errors_dict.append(series[y_test[i]])
+	return dict(Counter(errors_dict))
 
 path = "dataset"
 
@@ -24,7 +39,7 @@ listeseries = ["2567_House", "2956_Criminal_Minds", "1262_Lost", "175_The_Walkin
 "217_Game_of_Thrones", "1906_The_Vampire_Diaries"]
 
 
-series, (X_train, X_test, y_train, y_test) = load_data(path, nbclass=40, split=True, ratio=0.8)
+listeseries, (X_train, X_test, y_train, y_test) = load_data(path, nbclass=60, split=True, ratio=0.8)
 
 count_vect = CountVectorizer(analyzer='word', token_pattern=r'\w{1,}', stop_words=stopwords.words('english'), strip_accents='ascii')
 count_vect.fit(X_train + X_test)
@@ -37,18 +52,16 @@ test_y = encoder.fit_transform(y_test)
 xtrain_count = count_vect.transform(X_train)
 xtest_count = count_vect.transform(X_test)
 
-print(len(X_train), xtrain_count.shape)
-print(len(X_test), xtest_count.shape)
-
-
-bayes = naive_bayes.MultinomialNB()
-bayes.fit(xtrain_count, train_y)
-predictions = bayes.predict(xtest_count)
-
-accuracy = metrics.accuracy_score(predictions, test_y)
-print("accuracy: "accuracy)
+accuracy, predictions = train_model(naive_bayes.MultinomialNB(), xtrain_count, train_y, xtest_count, test_y)
 
 predictions = np.array(predictions)
 y_test = np.array(y_test)
 
-erreurs = predictions != y_test
+errors_dict = getErrors(predictions, y_test, listeseries)
+
+import pprint
+
+pp = pprint.PrettyPrinter(indent=4)
+
+pp.pprint(errors_dict)
+
