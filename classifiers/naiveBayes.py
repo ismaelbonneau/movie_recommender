d41967b36453156df7b6f415a911d3247255ac93
Ismael Bonneau <ismael95210@gmail.com>
2019-02-15 22:12:59 +0100

    ajout naive bayes

---
+++ classifiers/naiveBayes.py
@@ -0,0 +1,76 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*
+
+# Ismael Bonneau & Issam Benamara
+
+from collections import Counter
+from utils.featureExtraction import wordLemmatizer
+from sklearn import preprocessing, naive_bayes, metrics
+import numpy as np
+
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
+import operator
+import matplotlib.pyplot as plt
+
+class NaiveBayes:
+	"""
+
+	"""
+
+	def __init__(self):
+
+		self.classifier = naive_bayes.MultinomialNB()
+		self.encoder = preprocessing.LabelEncoder()
+
+
+	def fit(self, X_train, y_train, X_test, y_test):
+		"""
+		Entraine le classifieur et evalue sur les donnees de test
+		"""
+		#encoding des vecteurs d'expectation 
+
+		train_y = self.encoder.fit_transform(y_train)
+		test_y = self.encoder.fit_transform(y_test)
+		#entrainement du classifieur
+		self.classifier.fit(X_train, train_y)
+		#evaluation
+		self.predictions = self.classifier.predict(X_test)
+		accuracy = metrics.accuracy_score(self.predictions, test_y)
+
+		print("trained on {} samples, validated on {}.".format(X_train.shape[0], X_test.shape[0]))
+		print("accuracy: ", accuracy)
+
+		return self.predictions, accuracy
+
+
+	def getErrors(self, labels, series):
+
+
+		#au cas où
+		self.predictions = np.array(self.predictions)
+		labels = np.array(labels)
+		#les épisodes pour lesquels le classieur s'est trompé
+		erreurs = self.predictions != labels
+		errors_dict = {}
+		for i, v in enumerate(erreurs):
+			if v:
+				nom = " ".join(series[labels[i]].split("_")[1:])
+				if nom not in errors_dict:
+					errors_dict[nom] = 1
+				else:
+					errors_dict[nom] += 1
+
+		errors_dict = sorted(errors_dict.items(), key=operator.itemgetter(1), reverse=True)
+		names = [x[0] for x in errors_dict]
+		errors = [x[1] for x in errors_dict]
+		#bar chart du nombre d'erreur pour chaque série
+		plt.figure(figsize=(20,10))
+		plt.ylabel("nombre d'erreurs")
+		plt.title("nombre d'erreurs pour chaque serie")
+		plt.bar(range(len(errors_dict)), errors, width=0.35)
+		plt.xticks(range(len(errors_dict)), names)
+		plt.show()
+
+		return errors_dict
