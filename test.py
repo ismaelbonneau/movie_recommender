8b3ad554014df84ed523462d8b238cfd988a03c7
Ismael Bonneau <ismael95210@gmail.com>
2019-02-16 23:43:46 +0100

    ya kho

--- test.py
+++ test.py
@@ -30,26 +30,3 @@ print("countVector model:")
 NB = NaiveBayes()
 accuracy, predictions = NB.fit(xtrain_count, y_train, xtest_count, y_test)
 NB.getErrors(y_test, listeseries)
-
-
-#vectorisation des données sous forme de vecteur de tf-idf pour les bi gram
-tfidf_vect_ngram = TfidfVectorizer(analyzer='word', token_pattern=r'\w{1,}', ngram_range=(2,3), max_features=5000)
-tfidf_vect_ngram.fit(X_train + X_test)
-
-xtrain_tfidf_ngram =  tfidf_vect_ngram.transform(X_train)
-xtest_tfidf_ngram =  tfidf_vect_ngram.transform(X_test)
-
-print("tf-idf n-gram model:")
-accuracy, predictions = NB.fit(xtrain_tfidf_ngram, y_train, xtest_tfidf_ngram, y_test)
-NB.getErrors(y_test, listeseries)
-
-#vectorisation des données sous forme de vecteur de tf-idf pour les mots
-tfidf_vect = TfidfVectorizer(analyzer='word', token_pattern=r'\w{1,}', max_features=5000)
-tfidf_vect.fit(X_train + X_test)
-
-xtrain_tfidf =  tfidf_vect.transform(X_train)
-xtest_tfidf =  tfidf_vect.transform(X_test)
-
-print("tf-idf word model:")
-accuracy, predictions = NB.fit(xtrain_tfidf, y_train, xtest_tfidf, y_test)
-NB.getErrors(predictions, y_test, listeseries)
