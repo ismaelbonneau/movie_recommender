3b3d56e166406c5cc17bc656f4ab49aeefe4de7a
Ismael Bonneau <ismael95210@gmail.com>
2019-03-02 20:16:16 +0100

    ajout NEextractor

--- utils/featureExtraction.py
+++ utils/featureExtraction.py
@@ -17,10 +17,83 @@ from collections import Counter
 
 
 
+class NEextractor():
+    """
+    classe permettant d'extraire les personnages d'une série
+    """
+
+    def __init__(self):
+        """
+        """
+        pass
+    def extract(self, path_to_dataset, series, includeAll=True, globalcutoff=30, individualcutoff=10):
+        """
+        réalise l'extraction
+        globalcutoff: nombre de mots à retenir parmi les mots ayant un df de 1 en considerant tout le corpus
+        individualcutoff: nombre de mots à retenir parmi les mots ayant un df de 1 en considerant 2 docs
+        """
+        #chargement du corpus
+        customstopwords = stopwords.words('english') + ["yes","yeah","hmm","hey","nah","oh","uh","okay","okey","ye","hey","na","ca","ok","got","come","back","up"]
+        corpus = []
+        for serie in series:
+            listepisodes = glob.glob(path+"/"+serie+"/*/*.tokens") #texte lemmatisé!
+            text = ""
+            for episode in listepisodes:
+                with codecs.open(episode, "r", "utf-8") as file:
+                    text += " " + file.read()
+            corpus.append(text)
+
+            
+        vectorizer = TfidfVectorizer(lowercase=True, 
+                             binary=False,
+                             analyzer='word',
+                             use_idf = True,
+                             stop_words=customstopwords,
+                             max_df = 1)
+
+        X = vectorizer.fit_transform(corpus)
+        terms = vectorizer.get_feature_names()
+
+        words_per_serie = {}
+
+        for serie in range(len(corpus)):
+            feature_index = X[serie, :].nonzero()[1]
+            tfidf_scores = zip(feature_index, [X[serie, x] for x in feature_index])
+            doc_terms = []
+            for word, score in [(terms[i], score) for (i, score) in tfidf_scores]:
+                doc_terms.append((word, score))
+            important_terms = [word for word, score in sorted(doc_terms, key=lambda x: x[1], reverse=True)][:globalcutoff]
+            words_per_serie[series[serie]] = important_terms
+
+        for serie in range(len(corpus)):
+            vectorizer = TfidfVectorizer(lowercase=True,binary=False,analyzer='word',use_idf=True,stop_words=customstopwords,max_df=1)
+            X = vectorizer.fit_transform([corpus[serie], corpus[(serie+1)%(len(corpus))], corpus[(serie+2)%(len(corpus))]])
+            terms = vectorizer.get_feature_names()
+            
+            feature_index = X[0, :].nonzero()[1]
+            tfidf_scores = zip(feature_index, [X[0, x] for x in feature_index])
+            doc_terms = []
+            for word, score in [(terms[i], score) for (i, score) in tfidf_scores]:
+                doc_terms.append((word, score))
+            important_terms = [word for word, score in sorted(doc_terms, key=lambda x: x[1], reverse=True)][:individualcutoff]
+            words_per_serie[series[serie]] = list(set(words_per_serie[series[serie]]) | set(important_terms))
+
+        self.words = words_per_serie
+        return words_per_serie
+
+    def save(self, filename):
+    	"""
+		enregistrer le fichier de personnages sur le disque
+    	"""
+    	with open(filename, "w", encoding="utf-8") as file:
+    		for serie in self.words:
+    			for mot in self.words[serie]:
+    				file.write(mot+"\n")
+
 
 class wordLemmatizer():
 	"""
-
+	classe permettant de lemmatiser un texte
 	"""
 
 	def __init__(self, fromfile=False):
@@ -44,7 +117,7 @@ class wordLemmatizer():
 
 class wordFreqDict():
 	"""
-
+	classe permettant de lemmatiser un texte et de retourner un modèle BOW
 	"""
 
 	def __init__(self, fromfile=False, lemmatise=True, frecDict=True):
